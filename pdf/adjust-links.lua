9ba02c11352b5cafb4a48f921c0fb419787f9dba
Leo McArdle <leo@mca.is>
2024-09-27 14:52:36 +0000

    add examples to lua filter

--- pdf/adjust-links.lua
+++ pdf/adjust-links.lua
@@ -4,8 +4,12 @@ function Link(el)
   -- Fix relative links
   if id:sub(1,1) == "." then
     if id:find("#") then
+      -- before: "../foo.md#bar"
+      -- after: "#bar"
       id = id:match("(#.*)")
     else
+      -- before: "../foo.md"
+      -- after: "#foo"
       id = id:match(".*/(.*).md")
       id = "#" .. id
     end
@@ -13,6 +17,8 @@ function Link(el)
 
   -- Fix section header links
   if id:sub(1,1) == '#' then
+    -- before: "#2.1_foo_bar"
+    -- after: "#foo-bar"
     id = id:gsub("_", "-")
     id = id:gsub("^[^a-zA-Z]+", "")
     id = "#" .. id
